1b8e578e0be56d1d38fa29e20df645837dfb7795
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-02-18 20:12:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Sample data, and utilities, for experimenting with danish library data.
+
+Extracted/processed from library data released at [hack4dk](http://hack4.dk).
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bibdata',
+  packages = ['bibdata'],
+  version = '0.0.1',
+  description = 'Utility code, and sample data, for data science experiments with danish bibliographic data',
+  author = 'RasmusErik Voel Jensen',
+  author_email = 'pypi@solsort.com',
+  url = 'https://github.com/rasmuserik/bibdata',
+  download_url = 'https://github.com/rasmuserik/bibdata/archive/0.0.1.tar.gz',
+  keywords = ['bibliographic', 'example'],
+  classifiers = [],
+)
