10b8418e43039eff574c16a8518fb1501b6c3122
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-02-19 10:23:25 +0100

    published

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Sample data, and utilities, for experimenting with danish library data.
 
-Extracted/processed from library data released at [hack4dk](http://hack4.dk).
+Extracted/processed from library data released at hack4dk.
--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
   author = 'RasmusErik Voel Jensen',
   author_email = 'pypi@solsort.com',
   url = 'https://github.com/rasmuserik/bibdata',
+  package_data={'bibdata': ['content-first.json.gz', 'genre-space.npy', 'meta.json.gz']},
   download_url = 'https://github.com/rasmuserik/bibdata/archive/v0.0.1.tar.gz',
   keywords = ['bibliographic', 'example'],
+  python_requires='>=3',
   classifiers = [],
 )
